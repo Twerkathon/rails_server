ebfefe6c7997a1695a8eccc265f2a27eccbe8b2b
Lauren Fazah <laurenfazah@gmail.com>
2014-11-08 13:39:49 -0500

    raked seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+trip1 = Trip.create(climate: "hot", departure_city: "Boston", departure_country: "USA", start_date: '2014-09-25', end_date: '2014-10-11', is_flexible: true, food: ["pizza"], music: ['jazz'], activities: ["hiking"], user_id: 1)
