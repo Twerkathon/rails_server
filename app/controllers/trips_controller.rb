2ec5703e58dcaae6a172c0bc077175a0630cca7d
Lauren Fazah <laurenfazah@gmail.com>
2014-11-08 13:34:18 -0500

    created trip controller methods

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,3 +1,51 @@
 class TripsController < ApplicationController
 
-end
+	def index
+		@trips = Trip.all
+		render json @trips
+	end
+
+	def create
+		@trip = Trip.find(params[:id])
+		render json: @trip
+	end
+
+	def new
+		@trip = Trip.new(trip_params)
+	end
+
+	def show
+		@trip = Trip.new
+		render json: @trip
+	end
+
+	def edit	
+
+	end
+
+	def update
+		respond_to do |format|
+      if @trip.update(trip_params)
+        format.html { redirect_to trips_url, notice: 'Trip was successfully updated.' }
+        format.json { render :show, status: :ok, location: @trip }
+      else
+        format.html { render :edit }
+        format.json { render json: @trip.errors, status: :unprocessable_entity }
+      end
+    end
+	end
+
+	def destroy
+		@trip.destroy
+    respond_to do |format|
+      format.html { redirect_to trips_url, notice: 'Trip was successfully deleted.' }
+      format.json { head :no_content }
+    end
+	end
+
+	private
+	def trip_params
+			params.require(:trip).permit(:climate, :departure_city, :departure_country, :start_date, :end_date, :is_flexible, :food, :music, :activities, :user_id)
+	end
+
+end    
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :trips
-  
+
+  root 'trips#index'
 end
