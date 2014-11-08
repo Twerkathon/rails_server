ffe3d21e077ccd4b5ae120362e0433d04b5e439e
Lauren Fazah <laurenfazah@gmail.com>
2014-11-08 12:16:52 -0500

    created user and trip migrations and models

---
+++ db/migrate/20141108164752_create_trip.rb
@@ -0,0 +1,21 @@
+require 'date'
+
+class CreateTrip < ActiveRecord::Migration
+  def change
+    create_table :trips do |t|
+    	t.string :climate
+    	t.string :departure_city
+    	t.string :departure_country
+    	t.datetime :start_date
+    	t.datetime :end_date
+    	t.boolean :is_flexible, default: 0
+    	t.string :food, array: true, default: []
+    	t.string :music, array: true, default: []
+    	t.string :activities, array: true, default: []
+
+    	t.belongs_to :user
+
+    	t.timestamp
+    end
+  end
+end
